7eb5e780115c6a931fd221001e541568a69efc6d
DiegoBulhoes <diegobulhoesm@gmail.com>
2020-10-21 16:06:49 -0400

    Add env

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ use Mix.Config
 
 # Configure your database
 config :gestao_alunos, GestaoAlunos.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "project",
-  hostname: "postgres",
+  username: System.get_env("DB_USER"),
+  password: System.get_env("DB_PASSWORD"),
+  database: System.get_env("DB_NAME"),
+  hostname: System.get_env("DB_HOST"),
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
